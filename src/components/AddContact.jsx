48a2134cd040bee5c4b5c3101b20fc45ea68a685
mr-kargar <mr.kargar@gmail.com>
2022-02-01 01:43:25 +0330

    responsive

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -4,13 +4,14 @@ import { AiOutlineUserAdd } from 'react-icons/ai'
 function AddContact() {
     return (
         <div className="container bg-light mb-3  py-3 shadow-lg classAddContact border rounded">
-            <div className="d-flex  align-items-center mb-3">
-                <input type="name" className="form-control mx-2" placeholder="Enter name" id="name" />
-                <input type="phone" className="form-control mx-2" placeholder="Enter phone" id="family" />
-                <input type="email" className="form-control mx-2" placeholder="Enter email" id="email" />
+            <div className="d-flex flex-column flex-md-row align-items-center mb-3">
+                <input type="name" className="form-control mb-2 mb-md-0 mx-2" placeholder="Enter name" id="name" />
+                <input type="phone" className="form-control mb-2 mb-md-0 mx-2" placeholder="Enter phone" id="family" />
+                <input type="email" className="form-control mb-2 mb-md-0 mx-2" placeholder="Enter email" id="email" />
             </div>
-            <button className="btn btn-info d-flex align-items-center mx-auto  "> <AiOutlineUserAdd className='mx-3'/><span className="mx-3">Add Contact</span></button>
-
+            
+            <button className="btn btn-info d-flex align-items-center mx-auto "> <AiOutlineUserAdd className='mx-3'/><span className="mx-3">Add Contact</span></button>
+           
         </div>
     );
 }
